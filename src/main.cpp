b6cd7fc35ce5c831b10c0827846576b0a940c31d
Juan Rios <juandarr@gmail.com>
2019-02-16 01:01:29 -0500

    feat: complete first draft of main function. Pending addition of helper functions file and method to update measurements in the particle filter

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <json.hpp>
-//#include <particle_filter.h>
+#include <particle_filter.h>
 
 //Using namespace classes
 using nlohmann::json;
@@ -80,14 +80,31 @@ int main(){
 
                         pf.prediction(delta_t, sigma_pos, previous_velocity, previous_yawrate);
                     }
+
+                    //TODO completion of the rest of Particle filter logic
                     
-                     
+                    //end telemetry section         
                 }
+            } else {
+                string msg = "42[\"manual\",{}]";
+                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
             }
-        }
+        } // end websocket message if
+    }); // end h.onMessage
 
+    h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                            char *message, size_t length) {
+        ws.close();
+        std::cout << "Disconnected" << std:endl;
+    });
+
+    int port = 4567;
+    if (h.listen(port)) {
+        std::cout << "Listen to port " << port << std::endl;
+    } else {
+        std::cerr << "Failed to listen to port" << std::endl;
+        return -1;
     }
-    
-    )
 
+    h.run();
 }

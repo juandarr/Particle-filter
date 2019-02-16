a32447818b537077b1305600b64ceb90e4a3b19d
Juan Rios <juandarr@gmail.com>
2019-02-16 00:47:50 -0500

    feat: complete first version of method prediction in particle filter class

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -45,3 +45,34 @@ void ParticleFilter:: init (double x, double y, double theta, double std[]) {
 
 }
 
+void ParticleFilter::prediction(double delta_t, double std_pos[],
+                                double velocity, double yaw_rate) {
+    // Define variables to store predicted x, y and theta values
+    double value_x;
+    double value_y;
+    double value_theta;
+
+    for (int i = 0; i <particles.size(); ++i) {
+        // Stores particle i in variable
+        struct Particle p = particles[i];
+
+        // Predicts new values for x,y,z based on the bicycle motion model 
+        // using velocity and yaw rate values
+        value_x = p.x + (velocity/yaw_rate)*(std::sin(p.theta+yaw_rate*delta_t) -std::sin(p.theta));
+        value_y = p.y +(velocity/yaw_rate)*(std::cos(p.theta)-std::cos(p.theta+yaw_rate*delta_t));
+        value_theta = p.theta + yaw_rate*delta_t;
+
+        //Add noise around each predicted value using the standard deviation vector
+        //Normal gaussian distribution for x, y and theta
+        normal_distribution<double> dist_x(value_x, std_pos[0]);
+        normal_distribution<double> dist_y(value_y, std_pos[1]);
+        normal_distribution<double> dist_theta(value_theta, std_pos[2]);
+
+        // Updates x,y and theta values with the new predictions
+        particles[i].x = dist_x(gen);
+        particles[i].y = dist_y(gen);
+        particles[i].theta = dist_theta(gen);
+    }
+    
+}
+
